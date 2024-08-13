bf620dd5320d8a98ded7e029bab7451e66d2e8d4
Fabricio Damian Di Paola <111092778+FabricioDDev@users.noreply.github.com>
2024-08-13 11:20:26 -0300

    Add files via upload

---
+++ FirstYear/act1-tema1/act1-tema1/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+//void cargarArrayNotas(float notas[]);
+//void mostrarNotaRegistrada(float* ptr);
+//float calcularPromedio(float* ptr);
+//int mayoresPromedios(float* ptr, int promedio);
+//int main() {
+	//EJ1:Cargar las notas del primer parcial de los 78 estudiantes de un curso.
+	// Luego de cargar todas las notas :
+	// Pedir un número y mostrar por pantalla la nota registrada.
+	// Por ejemplo, se ingresa 10 para mostrar el décimo examen.
+	//- Listar cuántos estudiantes obtuvieron una nota mayor al promedio.
+
+	//float notas[10], *ptr_notas;
+	//cargarArrayNotas(notas);
+	//ptr_notas = notas;
+	//string opcion;
+	//float promedio = 0;
+	//int cantPromediosMayores = 0; 
+	//menu
+	//cout << "menu" << endl;
+	////cout << "0-salir";
+	//cout << "1-ver una nota de estudiante" << endl;
+	//cout << "2-ver nota promedio y cantidad de estudiantes que la superaron." << endl;
+	//cin >> opcion;
+	//while (opcion != "0") {
+	//	if (opcion == "1") {
+	//		mostrarNotaRegistrada(ptr_notas);
+	//	}
+	//	else if (opcion == "2") {
+	//		promedio = calcularPromedio(ptr_notas);
+	//		cantPromediosMayores = mayoresPromedios(ptr_notas, promedio);
+	//		cout << "la cantidad de " << cantPromediosMayores << " alumnos de 10 superaron el promedio" << endl;
+	//	}
+
+
+		//menu
+	//	cout << "menu" << endl;
+		//cout << "0-salir";
+		//cout << "1-ver una nota de estudiante" << endl;
+		//cout << "2-ver nota promedio y cantidad de estudiantes que la superaron." << endl;
+		//cin >> opcion;
+	//}
+
+	
+	//return 0;
+//}
+//void cargarArrayNotas(float notas[]) {
+	//for (int i = 0; i < 10; i++) {
+		//cout << "ingrese la nota del estudiante: " << i << endl;
+		//cin >> notas[i];
+	//}
+	//}
+//void mostrarNotaRegistrada(float *ptr) {
+	//int nroEstudiante;
+	//cout << "ingrese el numero de estudiante para ver la nota:" << endl;
+	//cin >> nroEstudiante;
+	//cout << "La nota del estudiante nro " << nroEstudiante << " es: " << ptr[(nroEstudiante-1)];
+//}
+//float calcularPromedio(float* ptr) {
+	//sumamos todas las notas.
+	//float suma = 0;
+	//float promedio;
+	//for (int i = 0; i < 10; i++) {
+	//	suma += ptr[i];
+	//}
+	//dividimos la suma por la cant total de notas.
+	//promedio = suma / 10;
+	//return promedio;
+//}
+//int mayoresPromedios(float* ptr, int promedio) {
+	//int mayoresPromedios = 0;
+	//for (int i = 0; i < 10; i++) {
+	//	if (ptr[i] > promedio) { mayoresPromedios++; }
+	//}
+	//return mayoresPromedios;
+//}
+
+int main() {
+	//2
+	//Hacer una función llamada quitarRepetidos que reciba dos vectores de enteros de 10 elementos llamados vectorSinProcesar y vectorSinRepetidos.La función debe procesar el vectorSinProcesar de manera que en el vectorSinRepetidos queden todos los elementos distintos del vectorSinProcesar.La función debe devolver la cantidad de elementos asignados a vectorSinRepetidos.
+	//	Ejemplo:
+	//vectorUno[10] = { 1, 2, 1, 2, 5, 5, 4, 4, 3, 3 }
+	//vectorDos[10];
+	//int elementos = quitarRepetidos(vectorUno, vectorDos);
+	//En elementos debe quedar el valor 5 ya que son 5 los elementos sin repetirse del vector.Además el vectorDos debe contener los valores 1, 2, 5, 4 y 3.
+
+
+	return 0;
+}
